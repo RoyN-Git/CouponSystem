bca5be95ff1a1eda81749889bce9b6f9e9f97e2d
shahar <jkunuy27@gmail.com>
2022-02-16 16:01:03 +0200

    remove todo from CustomerDAO

--- src/main/java/dao/CustomersDAO.java
+++ src/main/java/dao/CustomersDAO.java
@@ -1,11 +1,14 @@
 package dao;
 
+import beans.Customer;
+
+import java.util.List;
+
 public interface CustomersDAO {
-    //todo: Remove from comment when merging Customer class
     boolean isCustomerExists(String email, String password);
-    //void addCustomer(Customer customer);
-    //void updateCustomer(Customer customer);
+    void addCustomer(Customer customer);
+    void updateCustomer(Customer customer);
     void deleteCustomer(int customerId);
-    //List<Customer> getAllCustomers();
-    //Customer getOneCustomer(int customerId);
+    List<Customer> getAllCustomers();
+    Customer getOneCustomer(int customerId);
 }
