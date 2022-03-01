1f11d0c77841d5d78680ca33d6c3e95ae55380d7
golan <golank1@gmail.com>
2022-03-01 16:11:41 +0200

    start CouponExpirationDailyJob

--- src/main/java/jobs/CouponExpirationDailyJob.java
+++ src/main/java/jobs/CouponExpirationDailyJob.java
@@ -8,9 +8,21 @@ public class CouponExpirationDailyJob implements Runnable {
     private CouponsDAO couponsDAO;
     boolean quit=false;
 
+    public CouponExpirationDailyJob(CouponsDAO couponsDAO, boolean quit) {
+        this.couponsDAO = couponsDAO;
+        this.quit = quit;
+    }
+
     @Override
     public void run() {
         //todo: finish the job
+        while (!quit){
+
+        }
+    }
+
+    public void stop(){
+
     }
 }
 
