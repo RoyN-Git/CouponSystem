39d0f8c3184f58ea4c2f41152857746d6dd213e3
Roy <60190602+Roy-inw-cyber@users.noreply.github.com>
2022-03-01 12:37:17 +0200

    companyDBDAO_Test

--- src/main/java/facade/LoginManager.java
+++ src/main/java/facade/LoginManager.java
@@ -22,6 +22,38 @@ public class LoginManager {
     }
     //todo: Finish the login logic later
     /*
+    package.facade;
+
+import dbdao.CompaniesDBDAO;
+import dbdao.CouponsDBDAO;
+import dbdao.CustomersDBDAO;
+import dao.CompaniesDAO;
+import dao.CouponsDAO;
+import dao.CustomersDAO;
+import beans.ClientType;
+import exception.CouponSystemException;
+import facade.AdminFacade;
+import facade.ClientFacade;
+import facade.CompanyFacade;
+import facade.CustomerFacade;
+
+public class LoginManager {
+
+    private static LoginManager instance = new LoginManager();
+    private CustomersDAO customersDAO;
+    private CompaniesDAO companiesDAO;
+    private CouponsDAO couponsDAO;
+
+    public static LoginManager getInstance() {
+        return instance;
+    }
+
+    private LoginManager() {
+        customersDAO = new CustomersDBDAO();
+        companiesDAO = new CompaniesDBDAO();
+        couponsDAO = new CouponsDBDAO();
+    }
+
     public ClientFacade login(String email, String password, ClientType clientType) {
         ClientFacade clientFacade = null;
         switch (clientType) {
