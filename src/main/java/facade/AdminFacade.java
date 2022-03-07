6e3bdeae6822ee6c67741344fb10074b433420b5
Roy <60190602+RoyN-Git@users.noreply.github.com>
2022-03-07 14:27:07 +0200

    /

--- src/main/java/facade/AdminFacade.java
+++ src/main/java/facade/AdminFacade.java
@@ -62,27 +62,45 @@ public class AdminFacade extends ClientFacade{
     public List<Company> getAllCompanies(){
         return this.companiesDAO.getAllCompanies(DBmanager.GET_ALL_COMPANIES,new HashMap<>());
     }
-
+    /**
+     * This method returns one company from the database
+     * @return one company from all companies from the database.
+     */
     public Company getOneCompany(int companyId){
         return this.companiesDAO.getOneCompany(companyId);
     }
-
+    /**
+     * This method add a Custumer to the database
+     * @add Custumer to the database.
+     */
     public void addCustomer(Customer customer){
         this.customersDAO.addCustomer(customer);
     }
-
+    /**
+     * This method update a Customer to the database
+     * @update Customer to the database.
+     */
     public void updateCustomer(Customer customer){
         this.customersDAO.updateCustomer(customer);
     }
-
+    /**
+     * This method delete a Customer from the database
+     * @rdelete a Customer from the database.
+     */
     public void deleteCustomer(int customerId){
         this.customersDAO.deleteCustomer(customerId);
     }
-
+    /**
+     * This method returns a list of companies from the database
+     * @return list of companies from the database.
+     */
     public List<Customer> getAllCustomers(){
         return  this.customersDAO.getAllCustomers(DBmanager.GET_ALL_CUSTOMERS, new HashMap<>());
     }
-
+    /**
+     * This method returns One Customer from the database
+     * @return One Customer from the database.
+     */
     public Customer getOneCustomer(int customerId){
         return this.customersDAO.getOneCustomer(customerId);
     }
